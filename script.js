d726df15f22097b7ad81f93f8ddc855778bf1a15
Tommy <TommyCohn116@gmail.com>
2017-04-10 12:40:21 -0400

    Grab html elements in setup().

---
+++ script.js
@@ -0,0 +1,25 @@
+//Constants
+
+
+//Global Variables
+var page = {};
+
+//Classes
+
+
+//Functions
+function setup() {
+	page.massCenter = document.getElementById("massCenter");
+	page.timeRatio = document.getElementById("timeRatio");
+	page.gConstant = document.getElementById("gConstant");
+	page.iniPosX = document.getElementById("iniPosX");
+	page.iniPosY = document.getElementById("iniPosY");
+	page.iniVelX = document.getElementById("iniVelX");
+	page.iniVelY = document.getElementById("iniVelY");
+	page.graph = document.getElementById("graph");
+	page.animate = document.getElementById("animate");
+	page.canvas = document.getElementById("graphArea");
+}
+
+//Executed Code
+
