d18a3406c0d1f1ec459059e24646900dea8f96fa
Tommy <TommyCohn116@gmail.com>
2017-04-10 12:44:31 -0400

    Began setting up user input.

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ var page = {};
 
 //Functions
 function setup() {
+	console.log("FUNCTION CALL: setup()");
+
 	page.massCenter = document.getElementById("massCenter");
 	page.timeRatio = document.getElementById("timeRatio");
 	page.gConstant = document.getElementById("gConstant");
@@ -19,6 +21,39 @@ function setup() {
 	page.graph = document.getElementById("graph");
 	page.animate = document.getElementById("animate");
 	page.canvas = document.getElementById("graphArea");
+	page.numInputList = document.getElementsByClassName("numInput");
+}
+function inputSetup() {
+	console.log("FUNCTION CALL: inputSetup("+")");
+
+	document.addEventListener("mousemove", function(event) { mouseMoved(event); });
+	page.canvas.addEventListener("mousedown", function(event) { mousedown(event); });
+	document.addEventListener("mouseup", function(event) { mouseup(event); });
+	page.canvas.addEventListener("wheel", function(event) { wheel(event); });
+	page.canvas.addEventListener("mouseenter", function(event) { mouseEnterCanvas(event); });
+	page.canvas.addEventListener("mouseleave", function(event) { mouseLeaveCanvas(event); });
+
+	for(var i=0; i<page.numInputList.length; ++i) {
+		page.numInputList[i].addEventListener(focus, function() { this.select(); });
+	}
+}
+function mouseMoved(event) {
+
+}
+function mousedown(event) {
+
+}
+function mouseup(event) {
+
+}
+function wheel(event) {
+
+}
+function mouseEnterCanvas(event) {
+
+}
+function mouseLeaveCanvas(event) {
+
 }
 
 //Executed Code
